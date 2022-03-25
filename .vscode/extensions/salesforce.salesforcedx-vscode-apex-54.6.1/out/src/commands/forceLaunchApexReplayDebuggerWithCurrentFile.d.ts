eda81f4c811ca2b056afbe91ffa676df3cbe064c
Deepak <deepakavattootte2020@outlook.com>
2022-03-25 13:07:19 +1300

    commit

---
+++ .vscode/extensions/salesforce.salesforcedx-vscode-apex-54.6.1/out/src/commands/forceLaunchApexReplayDebuggerWithCurrentFile.d.ts
@@ -0,0 +1,7 @@
+import { SfdxCommandletExecutor } from '@salesforce/salesforcedx-utils-vscode/out/src';
+import { Command } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
+export declare function forceLaunchApexReplayDebuggerWithCurrentFile(): Promise<void>;
+export declare class ForceAnonApexLaunchReplayDebuggerExecutor extends SfdxCommandletExecutor<{}> {
+    build(): Command;
+    execute(): Promise<void>;
+}
---
+++ .vscode/extensions/salesforce.salesforcedx-vscode-apex-54.6.1/out/src/commands/index.d.ts
@@ -0,0 +1,6 @@
+export { ApexLibraryTestRunExecutor, forceApexDebugClassRunCodeActionDelegate, forceApexDebugMethodRunCodeActionDelegate, forceApexTestClassRunCodeAction, forceApexTestClassRunCodeActionDelegate, forceApexTestMethodRunCodeAction, forceApexTestMethodRunCodeActionDelegate } from './forceApexTestRunCodeAction';
+export { forceApexLogGet } from './forceApexLogGet';
+export { forceApexTestRun } from './forceApexTestRun';
+export { forceAnonApexDebug, forceAnonApexExecute } from './forceAnonApexExecute';
+export { forceApexTestSuiteAdd, forceApexTestSuiteCreate, forceApexTestSuiteRun } from './forceApexTestSuite';
+export { forceLaunchApexReplayDebuggerWithCurrentFile } from './forceLaunchApexReplayDebuggerWithCurrentFile';
